62cc544aa4925f32951b1e683eaba511e6c87031
Will Roever <william.roever@gmail.com>
2016-03-05 16:14:03 -0600

    updated launch script

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# select random service and run test
-RAND=$(shuf -i 1-2 -n 1);
+# select random service and run test 3-6 times
+RAND=$(shuf -i 1-4 -n 1);
 echo $RAND;
 
-if [ $RAND -eq "1" ];
+if [ $RAND -eq "1" ]
     then SVC="dropbox"
+elif [ $RAND -eq "2" ]
+    then SVC="box"
+elif [ $RAND -eq "3" ]
+    then SVC="google"
 else
-    SVC='box'
+    SVC="amazon"
 fi
 
-TEST="python /Users/Will/Desktop/MSCS/EECS\ 495\ -\ ISE/test.py $SVC 5"
-eval $TEST
+python '/Users/Will/Desktop/MSCS/EECS 495 - ISE/test.py' $SVC $(shuf -i 3-6 -n 1)
